0964b24dc0e1987b72da1e7ff45c4219808c06f6
汪培林 <1448456064@qq.com>
2018-11-25 19:57:04 +0800

    【+】数据库数据

---
+++ 数据/t_adminnews.sql
@@ -0,0 +1,8 @@
+INSERT INTO t_adminnews VALUES (1, 1, 1, '申请注册', '店铺江西菜馆', '2018-11-09 14:05:16');
+INSERT INTO t_adminnews VALUES (2, 1, 2, '申请注册', '店铺南京人家', '2018-11-09 14:07:42');
+INSERT INTO t_adminnews VALUES (3, 1, 3, '申请注册', '店铺河南菜馆', '2018-11-09 14:10:06');
+INSERT INTO t_adminnews VALUES (4, 1, 4, '申请注册', '店铺苏州小吃', '2018-11-01 14:12:11');
+INSERT INTO t_adminnews VALUES (5, 2, 1, '申请注册', '骑手', '2018-11-09 14:15:45');
+INSERT INTO t_adminnews VALUES (6, 2, 2, '申请注册', '骑手', '2018-11-09 14:16:50');
+INSERT INTO t_adminnews VALUES (7, 2, 3, '申请注册', '骑手', '2018-11-09 14:18:09');
+INSERT INTO t_adminnews VALUES (8, 2, 4, '申请注册', '骑手', '2018-11-01 14:19:21');
