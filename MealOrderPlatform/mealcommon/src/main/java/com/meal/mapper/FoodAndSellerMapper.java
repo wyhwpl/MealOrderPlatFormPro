72f2d0da481e93e75ce60903d0f0f13837da19b7
汪培林 <1448456064@qq.com>
2018-12-02 17:49:21 +0800

    【*】修改菜品信息mapper

--- MealOrderPlatform/mealcommon/src/main/java/com/meal/mapper/FoodAndSellerMapper.java
+++ MealOrderPlatform/mealcommon/src/main/java/com/meal/mapper/FoodAndSellerMapper.java
@@ -1,31 +1,11 @@
 package com.meal.mapper;
 
-import com.meal.pojo.Food;
-import com.meal.pojo.FoodExample;
-import org.apache.ibatis.annotations.Param;
+import com.meal.pojo.FoodAndSeller;
 
-import java.util.List;
 
 public interface FoodAndSellerMapper {
-    int countByExample(FoodExample example);
 
-    int deleteByExample(FoodExample example);
+    FoodAndSeller selectByPrimaryKey(Integer id);
 
-    int deleteByPrimaryKey(Integer id);
 
-    int insert(Food record);
-
-    int insertSelective(Food record);
-
-    List<Food> selectByExample(FoodExample example);
-
-    Food selectByPrimaryKey(Integer id);
-
-    int updateByExampleSelective(@Param("record") Food record, @Param("example") FoodExample example);
-
-    int updateByExample(@Param("record") Food record, @Param("example") FoodExample example);
-
-    int updateByPrimaryKeySelective(Food record);
-
-    int updateByPrimaryKey(Food record);
 }
